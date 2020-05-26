ecb642f156c4bcdc9695cb795aaea79d30ca7214
Jérémy Pingeon <pingeon.jeremy@icloud.com>
2020-05-26 14:23:05 +0200

    Add welcome message

--- resources/views/dash/home.blade.php
+++ resources/views/dash/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.dash')
 
 @section('content')
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">JustAuthMe Dash</h4>
+            <p>
+                Bienvenue sur la plateforme destinée aux développeurs, vous pouvez ici générer des identifiants pour intégrer JustAuthMe sur vos applications.<br /><br />
+                <a href="{{ url(action('Dash\AppsController@create')) }}" class="btn btn-secondary">Créer une application</a>
 
+            </p>
+            
+        </div>
+    </div>
 @endsection
